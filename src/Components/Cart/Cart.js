efaa34ae92d251b690c6c3cfd806b91f6e48a001
PulakBala <pulakbala0524@gmail.com>
2022-11-05 17:16:09 +0600

    conditional rendaring

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart,handleRemoveItem}) => {
+    let message;
+    if(cart.length === 0) {
+        message = <p>Please buy at least one item !!!</p>
+    } 
+    else{
+       message = <p>Thanks for buying </p>
+    }
     return (
         <div>
-          <h3>Order Summery</h3>
-          <h4>Order quantity : {cart.length}</h4>  
+          <h3 className={cart.length === 2 ? `orange` : `purple`}>Order Summery</h3>
+          <h4 className={`bold ${cart.length === 2 ? 'blue' : 'yellow'}`}>Order quantity : {cart.length}</h4>  
           {
             cart.map(tshirt => <p
-            
-            key={tshirt._id}
-
-
+                key={tshirt._id}
             > 
-            {tshirt.name}
-            <button onClick={()=>handleRemoveItem(tshirt)} >X</button>    
+                {tshirt.name}
+                <button onClick={()=>handleRemoveItem(tshirt)} >X</button>    
             </p>)
+          }
+          {
+            message
           }
         </div>
     );
