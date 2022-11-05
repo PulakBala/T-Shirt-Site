3ef3361d150ff0447193bcaed4fb9f4082236b06
PulakBala <pulakbala0524@gmail.com>
2022-11-05 15:19:15 +0600

    add t-shirt layout

---
+++ src/Components/Tshirt/Tshirt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tshir.css'
+const Tshirt = ({tshirt}) => {
+    const {picture, name, price} = tshirt;
+    return (
+        <div className='t-shirt'>
+            <img src={picture} alt="" />
+            <h2>Name : {name}</h2>
+            <h4>Price : {price}</h4>
+            <button>Buy this</button>
+        </div>
+    );
+};
+
+export default Tshirt;
